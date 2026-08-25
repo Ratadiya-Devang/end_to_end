59a8076a502f8f54e0bec631a3c164285ea8504b
Ratadiya Devang <ratadiyadevang3940@gmail.com>
2026-08-25 15:21:43 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import joblib 
+from fastapi import FastAPI 
+
+
+
+api = FastAPI()
+model = joblib.load("model.pkl")
+
+
+@api.get("/")
+def testapi():
+    return {"msg":"API run successfully"}
+
+
+@api.post("/placement_or_not")
+def predict_placement(marks:float):
+
+    new_data = pd.DataFrame({
+        "marks":[marks]
+    })
+    
+    result = model.predict(new_data)
+
+    result = "placement ok" if result == 1 else "no placement"
+
+    return {"msg": result}
